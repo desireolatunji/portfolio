5cf940627d731de3cded9cc7ba6b22f069fa4f6d
desireolatunji <desire.olatunji123@gmail.com>
2026-08-29 06:51:38 +0100

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Portfolio from "./components/portfolio";
+import "./Portfolio.css";
 
 
 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,4 @@
-import "./Portfolio.css";
+
 
 const  Portfolio=() => {
   return (
